20c3d678f5f3c27b8eff04a953e44f7c7d26560a
suryansh-codes15 <suryanshsrivastava215@gmail.com>
2026-02-12 04:23:31 +0530

    Update App component and UI improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,24 @@ import { AnimatePresence } from 'framer-motion'
 import Home from './pages/Home'
 import ServiceDetail from './pages/ServiceDetail'
 import ComingSoon from './pages/ComingSoon'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   const location = useLocation()
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/services/:id" element={<ServiceDetail />} />
-        <Route path="/coming-soon" element={<ComingSoon />} />
-        {/* Redirect unknown routes to Home for SPA behavior */}
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </AnimatePresence>
-  )
+    <>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/services/:id" element={<ServiceDetail />} />
+          <Route path="/coming-soon" element={<ComingSoon />} />
+          {/* Redirect unknown routes to Home for SPA behavior */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </AnimatePresence>
+      )
 }
 
-export default App
+      export default App
